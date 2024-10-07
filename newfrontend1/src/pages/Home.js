5e6fe9f4b2e49947dc60b0b864a1eab980f40ad4
Vikas Yadav <vcy@qad.com>
2024-10-08 00:37:23 +0530

    Added frontend components and initial setup

---
+++ newfrontend1/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to OM Shoe Marketplace</h2>
+            <p>Browse our collection of shoes!</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ newfrontend1/src/pages/Products.js
@@ -0,0 +1,13 @@
+// src/pages/Products.js
+import React from 'react';
+
+function Products() {
+    return (
+        <div>
+            <h2>Our Products</h2>
+            <p>Here you can find a variety of shoes to choose from.</p>
+        </div>
+    );
+}
+
+export default Products;
